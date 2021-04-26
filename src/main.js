c9a35843a3e83416dcf4fd0ae737acfff4f4760d
xxfan119 <1062234102@qq.com>
2021-04-26 16:13:56 +0800

    d

--- src/main.js
+++ src/main.js
@@ -19,12 +19,12 @@ const  baseSize  =  22;
     let  resizeEvt  =       "orientationchange"  in  window  ?  "orientationchange"  :  "resize",
             recalc  =   function ()  {       setRem();     };  
     if  (!doc.addEventListener)  return;  
-    win.addEventListener(resizeEvt,  recalc,  false);
+    // win.addEventListener(resizeEvt,  recalc,  false);
 })(document,  window);
 
 function  setRem()  {   // 当前页面宽度相对于 1920宽的缩放比例，可根据自己需要修改。
       
-    const  scale  =  document.documentElement.scrollHeight /  1080;   // 设置页面根节点字体大小（“Math.min(scale, 2)” 指最高放大比例为2，可根据实际业务需求调整）
+    const  scale  =  document.documentElement.clientHeight  /  1080;   // 设置页面根节点字体大小（“Math.min(scale, 2)” 指最高放大比例为2，可根据实际业务需求调整）
       
     document.documentElement.style.fontSize  =     baseSize  *  Math.min(scale,  5)  +  "px";
 }
